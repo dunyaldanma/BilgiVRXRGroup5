70e07869407f40157a66722fe84904e378708faa
dunyaldanma <166210836+dunyaldanma@users.noreply.github.com>
2025-04-15 22:24:58 +0300

    starting to add poke building block

--- Assets/Scripts/Poke.cs
+++ Assets/Scripts/Poke.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 public class Poke : MonoBehaviour
 {
 
-    public bool Button0 = false;
-    public bool Button1 = false;
-    public bool Button2 = false;
-    public bool Button3 = false;
+    public bool poked = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,60 +14,16 @@ public class Poke : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(poked);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (gameObject.tag == "Button0")
-        {
-            Debug.Log("BUTTON 0 ENTER");
-            Button0 = true;
-        }
-        
-        if (gameObject.tag == "Button1")
-        {
-            Debug.Log("BUTTON 1 ENTER");
-            Button1 = true;
-        }
-        
-        if (gameObject.tag == "Button2")
-        {
-            Debug.Log("BUTTON 2 ENTER");
-            Button2 = true;
-        }
-        
-        if (gameObject.tag == "Button3")
-        {
-            Debug.Log("BUTTON 3 ENTER");
-            Button3 = true;
-        }
+        poked = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (gameObject.tag == "Button0")
-        {
-            Debug.Log("BUTTON 0 RELEASE");
-            Button0 = false;
-        }
-
-        if (gameObject.tag == "Button1")
-        {
-            Debug.Log("BUTTON 1 RELEASE");
-            Button1 = false;
-        }
-
-        if (gameObject.tag == "Button2")
-        {
-            Debug.Log("BUTTON 2 RELEASE");
-            Button2 = false;
-        }
-
-        if (gameObject.tag == "Button3")
-        {
-            Debug.Log("BUTTON 3 RELEASE");
-            Button3 = false;
-        }
+        poked = false;
     }
 }
