e000d62eff4be16fb968555a21ca7de3348be6a4
dunyaldanma <166210836+dunyaldanma@users.noreply.github.com>
2025-06-05 11:58:12 +0300

    fix plank lvl
    no need to check tag it makes it not work cuz player obj is in different layer (named = player) so it can not detect it i guessssss.

--- Assets/0NewScripts/RePlank.cs
+++ Assets/0NewScripts/RePlank.cs
@@ -1,31 +1,30 @@
 using UnityEngine;
 
-public class PlankUnplacement : MonoBehaviour
+public class RePlank : MonoBehaviour
 {
-    [SerializeField] GameObject grab, plank;
-
+    [SerializeField] GameObject grab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.CompareTag("Player"))
-        //{
-            Debug.Log("triggvorked");
-            plank.tag = "Plank";
-            Rigidbody rb = plank.GetComponent<Rigidbody>();
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
             rb.constraints = RigidbodyConstraints.None;
+            other.gameObject.tag = "Untagged";
             grab.SetActive(true);
             Destroy(this.gameObject);
-        //}
+        }
     }
 }
